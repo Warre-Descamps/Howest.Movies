adab3b65f310abec5cd647c4e915e1dd3a8651e7
Magyx <w.magyx@gmail.com>
2024-04-24 22:52:32 +0200

    Add login and register menus

--- Howest.Movies.Sdk/IdentityDtos/Request.cs
+++ Howest.Movies.Sdk/IdentityDtos/Request.cs
@@ -2,6 +2,6 @@
 
 public class Request
 {
-    public required string Email { get; set; }
-    public required string Password { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
