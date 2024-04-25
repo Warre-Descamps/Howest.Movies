10d6ad89015c4379095fc54b1bfbbd5b429dc17b
Magyx <w.magyx@gmail.com>
2024-04-25 18:50:05 +0200

    Add "authenticationstate"

---
+++ Howest.Movies.WebApp/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Howest.Movies.Sdk.IdentityDtos.Results;
+
+namespace Howest.Movies.WebApp.Models;
+
+internal class User
+{
+    public string Email { get; set; } = null!;
+    public LoginResult? LoginResult { get; set; }
+}
