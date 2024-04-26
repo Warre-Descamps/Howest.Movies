05dbe284d35cc9bd69955f9e85910186ebf04b3c
Magyx <w.magyx@gmail.com>
2024-04-26 12:25:09 +0200

    Add ability to login

--- Howest.Movies.WebApp/Program.cs
+++ Howest.Movies.WebApp/Program.cs
@@ -2,6 +2,7 @@ using Howest.Movies.Sdk;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Howest.Movies.WebApp;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,7 +10,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services
-    .AddMudServices()
+    .AddMudServices(config =>
+    {
+        config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+    })
     .InstallMoviesSdk(builder.Configuration);
 
 await builder.Build().RunAsync();
